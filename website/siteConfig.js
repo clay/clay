f9ce4b64008c5cfc791ecbba0fcae123e6e54e71
Matt Oberle <matt.r.oberle@gmail.com>
2020-12-08 12:40:58 -0500

    updates CNAME/siteConfig for new temporary docs domain

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -23,8 +23,8 @@ const siteConfig = {
   //   baseUrl: '/test-site/',
 
   // Used for publishing and more
-  projectName: 'Clay',
-  organizationName: 'clayplatform',
+  projectName: 'clay',
+  organizationName: 'clay',
   // For top-level user or org sites, the organization is still the same.
   // e.g., for the https://JoelMarcey.github.io site, it would be set like...
   //   organizationName: 'JoelMarcey'
